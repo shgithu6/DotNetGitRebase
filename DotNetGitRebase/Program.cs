b67cf61f185559d53681fb9a9b24db76492ca176
shgithu6 <to.mohamedsheik@gmail.com>
2022-08-20 23:45:56 -0500

    Master Commit - 1

--- DotNetGitRebase/Program.cs
+++ DotNetGitRebase/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+MasterMethod();
+
+static void MasterMethod()
+{
+    Console.WriteLine("Initial Master Method");
+}
